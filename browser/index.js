9cd6a42cb773f90f04fe2f4304c506ee568d48d9
Rita Bulman <ritabulman@outlook.com>
2016-11-12 11:28:50 -0500

    made some questions to start out with

--- browser/index.js
+++ browser/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-import React from 'react'
+import React from 'react';
 import {Component} from 'react';
-import Root from './components/Root.js'
-import {render} from 'react-dom'
-import {Provider} from 'react-redux'
+import Root from './components/Root.js';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
 import { Router, Route, IndexRoute, IndexRedirect, hashHistory } from 'react-router';
-import store from './store'
-import Result from './components/Result'
+import store from './store';
+import Result from './components/Result';
 
 render (
   <Provider store={store}>
